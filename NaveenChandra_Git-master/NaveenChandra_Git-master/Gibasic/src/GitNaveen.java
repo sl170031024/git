64ca4f8a74152ac1acd37997faf3869c5f90c669
sl170031024 <60143147+sl170031024@users.noreply.github.com>
2020-07-21 08:13:13 +0530

    Add files via upload

---
+++ NaveenChandra_Git-master/NaveenChandra_Git-master/Gibasic/src/GitNaveen.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class GitNaveen {
+public static void main(String args[])
+{
+	Scanner n=new Scanner(System.in);
+	int a1=n.nextInt();
+	int b1=n.nextInt();
+	for(int i=a1;i<b1;i++)
+	{
+	if(i%2==0)
+	{
+	System.out.println(i);
+
+	}
+
+
+
+	
+}
+}
+}
